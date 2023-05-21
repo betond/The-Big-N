08e4edd8ffbb2a52bea9b1f19e8972f352e25c31
betond <beto971227@gmail.com>
2023-05-21 13:47:32 -0600

    imagenes

--- Practica4/src/index.py
+++ Practica4/src/index.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/signin')
 def signIn():
     return render_template('signIn.html')
 
 @app.route('/signup')
 def signUp():
     return render_template('signUp.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.run(debug=True)
